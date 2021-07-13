d4729ea66a56741251d9fbd4d7217c9511c95de4
jinchangyi <83002248+jinchangyi@users.noreply.github.com>
2021-07-13 21:12:18 +0800

    Add files via upload

--- lodash/jinchangyi-lodash.js
+++ lodash/jinchangyi-lodash.js
@@ -1,4 +1,4 @@
-var jinchangyi = function () {
+var jinchangyi = (function () {
   //将数组（array）拆分成多个 size 长度的区块，并将这些区块组成一个新数组。如果array 无法被分割成全部等长的区块，那么最后剩余的元素将组成一个区块。
   function chunk(array, size) {
     let arr = [],
@@ -106,4 +106,16 @@ var jinchangyi = function () {
     }
     return array;
   }
-};
+  return {
+    chunk: chunk,
+    compact: compact,
+    concat: concat,
+    reverse: reverse,
+    reduce: reduce,
+    reduceRight: reduceRight,
+    map: map,
+    forEach: forEach,
+    filter: filter,
+    fill: fill,
+  };
+})();
